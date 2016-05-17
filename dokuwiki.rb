021227c691efaafba1cf8c9aeb02a9e57792cee9
Iyamoto <Iyamoto@users.noreply.github.com>
2016-05-17 11:55:37 +0300

    bugging

--- dokuwiki.rb
+++ dokuwiki.rb
@@ -16,12 +16,13 @@ link '/var/www/html/wiki' do
   to '/usr/share/dokuwiki'
 end
 
-file '/etc/cron.daily/dokuwiki-backup.sh' do
-  content "#test"
+remote_file '/etc/cron.daily/dokuwiki-backup.sh' do
+  source 'https://raw.githubusercontent.com/Iyamoto/DokuWiki/master/backup.sh'
   owner 'root'
   group 'root'
   mode '0755'
-end
+  action :create
+end  
 
 service "apache2" do
   action :restart
